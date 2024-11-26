972fecab05127722e0689a0f1938daf0216c14de
hgalchi <97970505+hgalchi@users.noreply.github.com>
2024-11-26 23:05:51 +0900

    [Silver II] Title: 나무 자르기, Time: 488 ms, Memory: 119424 KB -BaekjoonHub

--- 백준/Silver/2805. 나무 자르기/나무 자르기.java
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.java
@@ -1,46 +1,47 @@
+
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-    static int[] trees;
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader((System.in)));
-        StringTokenizer st = new StringTokenizer(br.readLine());
 
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        st = new StringTokenizer(br.readLine());
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
 
-        trees = new int[N];
-        int max = 0;
-        int min = 0;
+        int[] arr = new int[N];
+        int high = 0,low=0;
+
         st = new StringTokenizer(br.readLine());
         for (int i = 0; i < N; i++) {
-            trees[i] = Integer.parseInt(st.nextToken());
-            max = Math.max(trees[i],max);
+            arr[i] = Integer.parseInt(st.nextToken());
+            high = Math.max(high, arr[i]);
         }
 
-        //이분탐색
-        while(min<max){
-            int mid = (min+max)/2;
-            long sum = cutting(mid);
+        while (low < high) {
+            int mid = (low + high) / 2;
+            long sum = 0;
+            for(int tree:arr){
+                //절단기 높이보다 나무 높이가 더 크다면
+                if (tree > mid) {
+                    sum += tree - mid;
+                }
+            }
+            // 잘린 합이 원하는 합보다 작거나 같은 경우
+            if (sum < M) {
+                //절단기의 길이를 줄인다.
+                high = mid;
+            } else {
+                low = mid + 1;
+            }
+        }
+        System.out.println(low-1);
+    }
 
-            if(sum < M){
-                max = mid;
-            }else{
-                min = mid+1;
-            }
-        }
-        System.out.println(min-1);
-    }
-    static long cutting(int height){
-        long result = 0;
-        for(int i=0;i<trees.length;i++){
-            if(trees[i] > height){
-                result += trees[i] - height;
-            }
-        }
-        return  result;
-    }
 }
