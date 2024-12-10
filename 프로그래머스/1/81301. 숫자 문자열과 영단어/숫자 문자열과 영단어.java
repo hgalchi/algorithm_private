120f17ba30b2dd36a852111825174ae8caeb0ab7
hgalchi <97970505+hgalchi@users.noreply.github.com>
2024-12-10 17:25:07 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 9.91 ms, Memory: 81.8 MB -BaekjoonHub

--- 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.java
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.java
@@ -2,11 +2,10 @@ import java.util.*;
 class Solution {
     public int solution(String s) {
         
-        String[] dist={"0","1","2","3","4","5","6","7","8","9"};
         String[] alpabat={"zero","one","two","three","four","five","six","seven","eight","nine"};
         
         for(int i=0;i<10;i++){
-            s=s.replace(alpabat[i],dist[i]);
+            s=s.replace(alpabat[i],""+i);
         }
         return Integer.parseInt(s);
     }
