8505f128647391cf1f35f75bc117e0738d47a6a1
hgalchi <97970505+hgalchi@users.noreply.github.com>
2024-12-10 17:22:08 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.11 ms, Memory: 78 MB -BaekjoonHub

--- 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.java
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.java
@@ -1,69 +1,13 @@
 import java.util.*;
 class Solution {
     public int solution(String s) {
-        StringBuilder sb=new StringBuilder();
         
-        int j=0;
-        for(int i=1;i<s.length()+1;i++)
-        {
-            //숫자인 경우 - '3'<'a'
-            if(s.charAt(j)<'a'){
-                sb.append(s.charAt(j));
-                j++;
-                continue;
-            }else{
-                //j부터 문자를 탐색
-            if(i-j>2){
-                String str=s.substring(j,i);
-                switch(str){
-                    case "zero":
-                        sb.append("0");
-                        j=i;
-                        break;
-                    case "one":
-                        sb.append("1");
-                        j=i;
-                        break;
-                    case "two":
-                        sb.append("2");
-                        j=i;
-                        break;
-                    case "three":
-                        sb.append("3");
-                        j=i;
-                        break;
-                    case "four":
-                        sb.append("4");
-                        j=i;
-                        break;
-                    case "five":
-                        sb.append("5");
-                        j=i;
-                        break;
-                    case "six":
-                        sb.append("6");
-                        j=i;
-                        break;
-                    case "seven":
-                        sb.append("7");
-                        j=i;
-                        break;
-                    case "eight":
-                        sb.append("8");
-                        j=i;
-                        break;
-                    case "nine":
-                        j=i;
-                        sb.append("9");
-                        break;
-                        
-                }
-            }
-            
-            }
-           
+        String[] dist={"0","1","2","3","4","5","6","7","8","9"};
+        String[] alpabat={"zero","one","two","three","four","five","six","seven","eight","nine"};
+        
+        for(int i=0;i<10;i++){
+            s=s.replace(alpabat[i],dist[i]);
         }
-        return Integer.parseInt(sb.toString()); 
-    
+        return Integer.parseInt(s);
     }
 }
