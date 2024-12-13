e37c1525719965f058499dffb26729fccf7e2bca
hgalchi <97970505+hgalchi@users.noreply.github.com>
2024-12-13 20:15:37 +0900

    [level 3] Title: 네트워크, Time: 2.50 ms, Memory: 86.4 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,35 +1,25 @@
 import java.util.*;
-
 class Solution {
-   
     public int solution(int n, int[][] computers) {
-        
-        boolean[] visited = new boolean[n];
-        Arrays.fill(visited,false);
-        int num = 0;
-
-        //모든 정점을 돌면서 방문하지 않은 정점을 탐색
-        for (int i = 0; i < n; i++) {
-            if (!visited[i]) {
-                Queue<Integer> q = new LinkedList<>();
+        boolean[] visited=new boolean[n];
+        int answer=0;
+      
+        for(int i=0;i<n;i++){
+            if(!visited[i]){
+                answer++;
+                Queue<Integer> q=new LinkedList<>();
                 q.offer(i);
-                visited[i]=true;
-                num++;
-                while (!q.isEmpty()) {
-                    int cur=q.poll();
-                    //방문하지 않았고 현재 정점과 간선으로 연결되어 있는 정점일 경우
-                    for (int j = 0; j < n; j++) {
-                        if (!visited[j] && computers[cur][j] == 1) {
-                            visited[j]=true;
-                            q.offer(j);
+                while(!q.isEmpty()){
+                    int row =q.poll();
+                    for(int col=0;col<n;col++){
+                        if(computers[row][col]==1&&!visited[col]){
+                            visited[row]=true;
+                            q.offer(col);
                         }
                     }
                 }
             }
-
-
-        }
-        System.out.println(num);
-        return num;
+            
+        }return answer;
     }
 }
