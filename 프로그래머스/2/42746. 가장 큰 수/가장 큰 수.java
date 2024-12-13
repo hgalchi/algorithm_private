902d7f9ae7a1983e14e8b4a70bb50f8edbc8ed55
hgalchi <97970505+hgalchi@users.noreply.github.com>
2024-12-13 14:20:07 +0900

    [level 2] Title: 가장 큰 수, Time: 202.66 ms, Memory: 135 MB -BaekjoonHub

---
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
@@ -0,0 +1,23 @@
+import java.util.*;
+class Solution {
+    public String solution(int[] numbers) {
+        String[] arr=new String[numbers.length];
+        
+        for(int i=0;i<arr.length;i++){
+            arr[i]=String.valueOf(numbers[i]);
+        }
+        
+        Arrays.sort(arr,(x,y)->(y+x).compareTo(x+y));
+        
+        if(arr[0].equals("0")){
+            return "0";
+        }
+        
+        StringBuilder answer=new StringBuilder();
+        
+        for(int i=0;i<arr.length;i++){
+            answer.append(arr[i]);
+        }
+        return answer.toString();
+    }
+}
