222ee85f50a9a31591173fe33397b122a2fdeb65
sunmmi <97970505+hgalchi@users.noreply.github.com>
2025-04-01 10:25:40 +0900

    [level 1] Title: 택배 상자 꺼내기, Time: 0.03 ms, Memory: 80.4 MB -BaekjoonHub

--- 프로그래머스/1/389478. 택배 상자 꺼내기/택배 상자 꺼내기.java
+++ 프로그래머스/1/389478. 택배 상자 꺼내기/택배 상자 꺼내기.java
@@ -1,28 +1,17 @@
 import java.util.*;
 class Solution {
     public int solution(int n, int w, int num) {
-        int next=0;
-        int cur=num;
-        int result=0;
+        int cnt=0;
         int[] list=new int[w];
         int dump=-1;
-        int dis = 0;
         for(int i=0;i<w;i++){
             list[i]=dump+2;
             dump=list[i];
         }
-        while(cur<=n){
-            //행이 짝수, 홀수
-            if((cur-1/w)%2==0){
-                dis = (w - 1) - ((cur - 1) % w);
-                next=cur+list[dis];
-            }else{
-                dis = (w - 1) - ((cur - 1) % w);
-                next=cur+list[dis];
-            }
-            cur=next;
-            result++;
+        while(num<=n){
+            num+=list[(w - 1) - ((num - 1) % w)];
+            cnt++;
         }
-        return result;
+        return cnt;
     }
 }
