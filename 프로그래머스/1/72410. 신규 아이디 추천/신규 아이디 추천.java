634aef19f82a0c3f032b24542b5d9ef7e88a5507
sunmmi <97970505+hgalchi@users.noreply.github.com>
2025-04-15 14:13:51 +0900

    [level 1] Title: 신규 아이디 추천, Time: 4.94 ms, Memory: 87.3 MB -BaekjoonHub

--- 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.java
+++ 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.java
@@ -1,72 +1,35 @@
-import java.util.*;
 class Solution {
     public String solution(String new_id) {
-         List<Character> list=new ArrayList<>();
-            for(char c:new_id.toCharArray()){
-                list.add(c);
+        String answer="";
+//        1번째 
+        new_id=new_id.toLowerCase();
+//         2번째
+        new_id=new_id.replaceAll("[^a-z0-9-_.]","");
+//         3번째
+        new_id=new_id.replaceAll("\\.+","\\.");
+//         4번째 
+        if(!new_id.isEmpty()&&new_id.charAt(0)=='.'){
+            new_id=new_id.substring(1);
+        }
+        if(!new_id.isEmpty()&&new_id.charAt(new_id.length()-1)=='.'){
+            new_id=new_id.substring(0,new_id.length()-1);
+        }
+//         5번째
+        if(new_id.length()==0){
+            new_id="a";
+        }
+//         6번째 
+        if(new_id.length()>=16){
+            new_id=new_id.substring(0,15);
+            if(!new_id.isEmpty()&&new_id.charAt(new_id.length()-1)=='.'){
+                new_id=new_id.substring(0,new_id.length()-1);
             }
-
-
-            // 1번째
-            for(int i=0;i<list.size();i++){
-                if(Character.isUpperCase(list.get(i))){
-                    list.set(i, Character.toLowerCase(list.get(i)));
-                }
-            }
-
-            //2번째
-            for(int i=list.size()-1;i>=0;i--){
-                Character cur=list.get(i);
-                if(!(Character.isAlphabetic(cur)||Character.isDigit(cur)
-                        ||cur=='-'||cur=='_'||cur=='.')){
-                    list.remove(i);
-                }
-            }
-
-
-            //3번째
-            int index=list.size()-1;
-            while(index>=0){
-                if(list.get(index)=='.'){
-                    int j=0;
-                    for(j=index-1;j>=0;j--){
-                        if(list.get(j)=='.'){
-                            list.remove(j);
-                        }else{
-                            break;
-                        }
-                    }
-                    index = j;
-                }else{
-                    index--;
-                }
-            }
-
-//         4번째
-            if(!list.isEmpty()&&list.get(0)=='.') list.remove(0);
-            if(!list.isEmpty()&&list.get(list.size()-1)=='.') list.remove(list.size()-1);
-           
-
-            // 5번째
-            if(list.isEmpty()) list.add('a');
-
-            // 6번째
-            if(list.size()>=16){
-                list=list.subList(0,15);
-                if(list.get(list.size()-1)=='.'){
-                    list.remove(list.size()-1);
-                }
-            }
-
-            // 7번째
-            while(list.size()<3){
-                list.add(list.get(list.size()-1));
-            }
-
-            StringBuilder sb=new StringBuilder();
-            for(char c:list){
-                sb.append(Character.toString(c));
-            }
-            return sb.toString();
+        }
+//         7번째
+        while(new_id.length()<3){
+            String last=Character.toString(new_id.charAt(new_id.length()-1));
+            new_id+=last;
+        }
+        return new_id;
     }
 }
