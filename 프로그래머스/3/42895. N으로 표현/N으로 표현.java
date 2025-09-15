0fbfc04bb0e1adc447bc71e02aff252ca593eab5
sunmmi <97970505+hgalchi@users.noreply.github.com>
2025-09-15 21:49:22 +0900

    [level 3] Title: N으로 표현, Time: 5.79 ms, Memory: 89 MB -BaekjoonHub

--- 프로그래머스/3/42895. N으로 표현/N으로 표현.java
+++ 프로그래머스/3/42895. N으로 표현/N으로 표현.java
@@ -1,34 +1,28 @@
 import java.util.*;
 class Solution {
+    static int N,number,min;
     public int solution(int N, int number) {
-       List<Set<Integer>> box=new ArrayList<>();
-        for(int i=0;i<=8;i++){
-            box.add(new HashSet<Integer>());
+        Solution.N=N;
+        Solution.number=number;
+        min=Integer.MAX_VALUE;
+        dfs(0,0);
+        return min>8?-1:min;
+    }
+    public void dfs(int cur,int count){
+        if(count>8) return;
+        if(cur==number){
+            min=Math.min(min,count);
+            return;
         }
-        //n을 한번 사용했을 경우
-        box.get(1).add(N);
-        
-        //n을 i번 사용했을 때 나오는 값의 경우의 수를 연산
-        for(int i=2;i<=8;i++){
-            for(int j=1;j<i;j++){
-                for(int num1:box.get(j)){
-                    for(int num2:box.get(i-j)){
-                        box.get(i).add(num1+num2);
-                        box.get(i).add(num1-num2);
-                        box.get(i).add(num1*num2);
-                        if(num1!=0&&num2!=0){
-                            box.get(i).add(num1/num2);
-                        }
-                    }
-                }
-            }
-            box.get(i).add(Integer.parseInt((""+N).repeat(i)));
+       
+        int concat=0;
+        for(int i=0;i<8-count;i++){
+            concat=concat*10+N;
+            dfs(cur+concat,count+1+i);
+            dfs(cur-concat,count+1+i);
+            dfs(cur *concat,count+1+i);
+            if(concat!=0) dfs(cur/concat,count+1+i);
         }
         
-        for(int i=1;i<=8;i++){
-            if(box.get(i).contains(number)){
-                return i;
-            }
-        }return -1;
     }
 }
